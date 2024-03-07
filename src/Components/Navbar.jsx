fe4619c8231039cc0f1f0621fb4880edd8a76592
Kalpesh <kalpeshdhoke7@gmai.com>
2024-03-07 17:59:45 +0530

    implement searchbar in nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,23 @@
+import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
   return (
-    <header className="grid grid-cols-2 justify-items-center items-center max-w-full font-medium">
+    <header className="grid grid-cols-3 justify-items-center items-center max-w-full font-medium">
       <div>
         <img src={logo} alt="Logo" className='w-32' />
       </div>
       <nav>
-        <ul className="grid grid-cols-5">
-          <li><a className='hover:text-pink' href="">Home</a></li>
-          <li><a className='hover:text-pink' href="">Pages</a></li>
-          <li><a className='hover:text-pink' href="">Products</a></li>
-          <li><a className='hover:text-pink' href="">Shop</a></li>
-          <li><a className='hover:text-pink' href="">Contact</a></li>
+        <ul className="grid grid-cols-5 cursor-pointer">
+          <li><Link className='hover:text-pink' to="/">Home</Link></li>
+          <li><a className='hover:text-pink'>Pages</a></li>
+          <li><a className='hover:text-pink'>Products</a></li>
+          <li><a className='hover:text-pink'>Shop</a></li>
+          <li><a className='hover:text-pink'>Contact</a></li>
         </ul>
       </nav>
+      <SearchBar />
     </header>
   )
 }
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,32 @@
+const SearchBar = () => {
+  return (
+    <div
+      className="relative flex outline">
+      <input
+        type="search"
+        className="bg-background p-2"
+        style={{ outlineOffset: 0 }}
+        placeholder='Search' />
+      <button
+        className="bg-pink p-3"
+        type="button"
+      >
+        <span className="[&>svg]:h-5 [&>svg]:w-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>
+        </span>
+      </button>
+    </div>
+  )
+}
+
+export default SearchBar
