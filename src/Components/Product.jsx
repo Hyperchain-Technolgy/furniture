dd79047d1dda380b2a9c64561ae2b5071203c069
Kalpesh <kalpeshdhoke7@gmai.com>
2024-03-12 15:03:30 +0530

    implement products

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,15 +1,20 @@
 import cart from '../assets/cart.svg'
 import heart from '../assets/heart.svg'
 
-const Product = () => {
+const Product = ({ src, price }) => {
   return (
-    <section className="border h-96 w-72">
-      <div>
-        <img src={cart} alt="cart icon" />
-        <img src={heart} alt="heart icon" />
+    <section className="border w-72 min-w-64 group hover:cursor-pointer">
+      <section className='group-hover:bg-gray-100'>
+        <div className='flex gap-4 p-5'>
+          <img src={cart} alt="cart icon" />
+          <img src={heart} alt="heart icon" />
+        </div>
+        <img src={src} alt="chair" className='w-44 mx-auto p-5' />
+      </section>
+      <div className='font-medium group-hover:bg-foreground duration-300 p-3'>
+        <p className='text-pink group-hover:text-white'>Cantislever chair</p>
+        <p className='group-hover:text-white duration-100 text-lg font-semibold'>₹{price}</p>
       </div>
-      <div></div>
-      <div></div>
     </section>
   )
 }
--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,11 +1,12 @@
 import Product from "./Product"
+import chair from '../assets/chair.png'
 
 const ProductsList = () => {
   return (
-    <section className="container mx-auto px-10 flex flex-col gap-2 items-center">
-      <Product />
-      <Product />
-      <Product />
+    <section className="container mx-auto px-10 flex flex-col gap-5 items-center sm:flex-row justify-center">
+      <Product src={chair} price={4999} />
+      <Product src={chair} price={4999} />
+      <Product src={chair} price={4999} />
     </section>
   )
 }
