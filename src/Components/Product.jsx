1381fa1d7cf410f6d6908a5b18cd5a1513969a16
Kalpesh <kalpeshdhoke7@gmai.com>
2024-03-13 12:58:08 +0530

    implement temp data & made changes accordingly

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -2,20 +2,22 @@ import cart from '../assets/cart.svg'
 import heart from '../assets/heart.svg'
 import SecondaryButton from './SecondaryButton'
 
-const Product = ({ src, price }) => {
+const Product = ({ data }) => {
   return (
     <section className="border w-72 min-w-64 group hover:cursor-pointer" >
-      <section className='group-hover:bg-gray-100'>
-        <div className='flex gap-4 p-5'>
+      <section>
+        <div className='flex gap-4 p-5 absolute'>
           <SecondaryButton src={cart} alt="cart icon" />
           <SecondaryButton src={heart} alt="heart icon" />
         </div>
-        <img src={src} alt="chair" className='w-44 mx-auto p-5' />
+        <div className='relative overflow-hidden bg-cover bg-no-repeat'>
+          <img src={data.image} alt="chair" className='transition duration-300 ease-in-out hover:scale-110' />
+        </div>
       </section>
-      <div className='font-medium group-hover:bg-foreground duration-300 p-3'>
-        <p className='text-pink group-hover:text-white'>Cantislever chair</p>
+      <div className='font-medium group-hover:bg-foreground duration-300 p-3 ease-in-out'>
+        <p className='text-pink group-hover:text-white'>{data.title}</p>
         <div className='flex gap-2 items-center'>
-          <p className='group-hover:text-white duration-100 text-lg font-semibold'>₹{price}</p>
+          <p className='group-hover:text-white duration-100 text-lg font-semibold'>₹{data.price}</p>
           <p className='text-gray-400 duration-100 text-lg line-through'>₹3999</p>
           <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-pink ring-1 ring-inset ring-red-600/10 group-hover:bg-blue-900 group-hover:ring-blue-50/10 group-hover:text-white">{'-20%'}</span>
         </div>
--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,12 +1,12 @@
 import Product from "./Product"
-import chair from '../assets/chair.png'
+import { data as furnituresData } from '../furnituresData'
 
 const ProductsList = () => {
   return (
     <section className="container mx-auto px-10 flex flex-col gap-5 items-center sm:flex-row justify-center">
-      <Product src={chair} price={4999} />
-      <Product src={chair} price={4999} />
-      <Product src={chair} price={4999} />
+      {furnituresData.map(data => (
+        <Product key={data.id} data={data} />
+      ))}
     </section>
   )
 }
