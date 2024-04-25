7eeaf768fd6095fe05c6ad40c6809f29ffcde0aa
Kalpesh <kalpeshdhoke7@gmail.com>
2024-04-25 16:07:01 +0530

    some aditional details for product

--- src/Components/CurrentProduct.jsx
+++ src/Components/CurrentProduct.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import { RadioGroup, Tab } from '@headlessui/react'
 import { StarIcon } from '@heroicons/react/20/solid'
 import { HeartIcon } from '@heroicons/react/24/outline'
@@ -26,8 +26,66 @@ const data = {
         'Water-resistant',
       ],
     },
+    {
+      name: "Care",
+      items: [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "Lorem ipsum dolor sit amet adipisicing elit",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "Lorem ipsum dolor sit amet consectetur elit",
+      ]
+    },
+    {
+      name: "Shipping",
+      items: [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "Lorem ipsum dolor sit amet adipisicing elit",
+      ]
+    }
   ],
 }
+
+const reviews = {
+  average: 4,
+  featured: [
+    {
+      id: 1,
+      rating: 5,
+      content: `
+        <p>This icon pack is just what I need for my latest project. There's an icon for just about anything I could ever need. Love the playful look!</p>
+      `,
+      date: 'July 16, 2021',
+      datetime: '2021-07-16',
+      author: 'Emily Selman',
+      avatarSrc:
+        'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    },
+    {
+      id: 2,
+      rating: 5,
+      content: `
+        <p>Blown away by how polished this icon pack is. Everything looks so consistent and each SVG is optimized out of the box so I can use it directly with confidence. It would take me several hours to create a single icon this good, so it's a steal at this price.</p>
+      `,
+      date: 'July 12, 2021',
+      datetime: '2021-07-12',
+      author: 'Hector Gibbons',
+      avatarSrc:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    },
+  ],
+}
+const faqs = [
+  {
+    question: 'What format are these icons?',
+    answer:
+      'The icons are in SVG (Scalable Vector Graphic) format. They can be imported into your design tool of choice and used directly in code.',
+  },
+  {
+    question: 'Can I use the icons at different sizes?',
+    answer:
+      "Yes. The icons are drawn on a 24 x 24 pixel grid, but the icons can be scaled to different sizes as needed. We don't recommend going smaller than 20 x 20 or larger than 64 x 64 to retain legibility and visual balance.",
+  },
+]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -175,7 +233,105 @@ const CurrentProduct = ({ product }) => {
                 </button>
               </div>
             </form>
+
+            {data.details.map((element) => (
+              <div key={element.name} className="mt-10 border-t border-gray-200 pt-10">
+                <h3 className="text-sm font-medium text-gray-900">{element.name}</h3>
+                <div className="prose prose-sm mt-4 text-gray-500">
+                  <ul role="list">
+                    {element.items.map((item) => (
+                      <li key={item}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
           </div>
+        </div>
+        <div className="mx-auto mt-16 w-full max-w-2xl lg:col-span-4 lg:mt-0 lg:max-w-none">
+          <Tab.Group as="div">
+            <div className="border-b border-gray-200">
+              <Tab.List className="-mb-px flex space-x-8">
+                <Tab
+                  className={({ selected }) =>
+                    classNames(
+                      selected
+                        ? 'border-indigo-600 text-indigo-600'
+                        : 'border-transparent text-gray-700 hover:border-gray-300 hover:text-gray-800',
+                      'whitespace-nowrap border-b-2 py-6 text-sm font-medium'
+                    )
+                  }
+                >
+                  Customer Reviews
+                </Tab>
+                <Tab
+                  className={({ selected }) =>
+                    classNames(
+                      selected
+                        ? 'border-indigo-600 text-indigo-600'
+                        : 'border-transparent text-gray-700 hover:border-gray-300 hover:text-gray-800',
+                      'whitespace-nowrap border-b-2 py-6 text-sm font-medium'
+                    )
+                  }
+                >
+                  FAQ
+                </Tab>
+              </Tab.List>
+            </div>
+            <Tab.Panels as={Fragment}>
+              <Tab.Panel className="-mb-10">
+                <h3 className="sr-only">Customer Reviews</h3>
+
+                {reviews.featured.map((review, reviewIdx) => (
+                  <div key={review.id} className="flex space-x-4 text-sm text-gray-500">
+                    <div className="flex-none py-10">
+                      <img src={review.avatarSrc} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
+                    </div>
+                    <div className={classNames(reviewIdx === 0 ? '' : 'border-t border-gray-200', 'py-10')}>
+                      <h3 className="font-medium text-gray-900">{review.author}</h3>
+                      <p>
+                        <time dateTime={review.datetime}>{review.date}</time>
+                      </p>
+
+                      <div className="mt-4 flex items-center">
+                        {[0, 1, 2, 3, 4].map((rating) => (
+                          <StarIcon
+                            key={rating}
+                            className={classNames(
+                              review.rating > rating ? 'text-yellow-400' : 'text-gray-300',
+                              'h-5 w-5 flex-shrink-0'
+                            )}
+                            aria-hidden="true"
+                          />
+                        ))}
+                      </div>
+                      <p className="sr-only">{review.rating} out of 5 stars</p>
+
+                      <div
+                        className="prose prose-sm mt-4 max-w-none text-gray-500"
+                        dangerouslySetInnerHTML={{ __html: review.content }}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </Tab.Panel>
+
+              <Tab.Panel className="text-sm text-gray-500">
+                <h3 className="sr-only">Frequently Asked Questions</h3>
+
+                <dl>
+                  {faqs.map((faq) => (
+                    <Fragment key={faq.question}>
+                      <dt className="mt-10 font-medium text-gray-900">{faq.question}</dt>
+                      <dd className="prose prose-sm mt-2 max-w-none text-gray-500">
+                        <p>{faq.answer}</p>
+                      </dd>
+                    </Fragment>
+                  ))}
+                </dl>
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ export default {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
     // require('flowbite/plugin')({
     //   charts: true,
     // }),
